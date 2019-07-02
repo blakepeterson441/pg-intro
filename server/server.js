b988f96c66333e7df03d4112cf57c72a9df5086d
Blake <blake.peterson.441@gmail.com>
2019-07-02 10:23:09 -0500

    server started

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Setting up our express app
+const app = express();
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Routes will go here
+
+
+// Start up the server
+const PORT = process.env.PORT || 5000;
+app.listen( PORT, () =>{
+    console.log(`Listening on port ${PORT}...`);
+});
